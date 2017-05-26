54b4413877399158571aaf6979035742c1670cb9
camduong <camduongsgu@gmail.com>
2017-05-26 13:51:13 +0700

    lần 3
    xóa pug, sửa thấy mẹ

--- Vacation/Vacation/Global.asax.cs
+++ Vacation/Vacation/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using Pug.ViewEngine;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +7,12 @@ using System.Web.Routing;
 
 namespace Vacation
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            
-            ViewEngines.Engines.Add(new PugViewEngine()); // then this to add PugViewEngine
-        }
-    }
+		public class MvcApplication : System.Web.HttpApplication
+		{
+				protected void Application_Start()
+				{
+						AreaRegistration.RegisterAllAreas();
+						RouteConfig.RegisterRoutes(RouteTable.Routes);
+				}
+		}
 }
